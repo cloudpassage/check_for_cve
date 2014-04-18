d8b770d9db11d3cd529b61dd4df894cb1389abaa
Eric Hoffmann <ehoffmann@cloudpassage.com>
2014-04-17 23:50:58 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+#CloudPassage Check For CVE Example
+
+Version: *1.0*
+<br />
+Author: *Eric Hoffmann* - *ehoffmann@cloudpassage.com*
+
+Users can use the provided example script to check for the presence of any individual CVE or list of CVEs. It uses the Halo API to get the details of the last scheduled or manually launched SVA scan for all active servers. It then checks for the presence of the provided CVE(s) in the scan findings.
+
+##Requirements and Dependencies
+
+To run, this script requires
+
+* Ruby installed on the host that runs the scripts.
+* Ruby gems: optparse, oauth2, rest-client, json
+* A read-only Halo API key/secret stored in a yaml file
+* The location of the yaml file set as a ENV variable
+
+##List of Files
+
+* **check_for_cves.rb**  - Ruby script which leverages the Halo API to check for the presence of various CVEs
+* **README.md**  -  This ReadMe file
+* **LICENSE.txt**  -  License from CloudPassage
+
+##Usage
+
+1. Copy a read-only Halo API key/secret from the Halo Portal into a "dot" file ie ~/.halo
+2. Set the location of the api-key file as a ENV variable called HALO_API_KEY_FILE
+3. Execute the script
+
+The format of ~/.halo
+```
+halo:
+  key_id : XXXXXXXX
+  secret_key : XXXXXXXXXXXXXXXXXXXXXXXXXXX
+```
+
+The additional variable in your ~/.bash_profile
+```
+HALO_API_KEY_FILE="/home/<your username>/.halo"
+export HALO_API_KEY_FILE
+```
+
+How to excute the script
+```
+ruby check_for_cves.rb --cve 'CVE-2010-0624,CVE-2011-4623'
+ip-10-123-254-12, 53.215.74.1, centos,6.5, cpio.x86_64, 2.10-11.el6_3, CVE-2010-0624
+ip-10-10-254-13, 54.192.200.254, centos,6.2, cpio.x86_64, 2.10-11.el6_3, CVE-2010-0624
+ip-10-10-254-13, 54.192.200.254, centos,6.2, rsyslog.x86_64, 4.6.2-12.el6, CVE-2011-4623
+Checked 2 servers for CVE-2010-0624,CVE-2011-4623
+```
---
+++ check_for_cves.rb
@@ -0,0 +1,98 @@
+# example script to display all server instances
+# where a given CVE was identified
+# Eric Hoffmann <ehoffmann@cloudpassage.com>
+
+require 'optparse'
+require 'oauth2'
+require 'rest-client'
+require 'json'
+
+# API helper class
+class API
+  def initialize (key_id, secret_key, host)
+    client = OAuth2::Client.new(key_id, secret_key,
+                  :site => "https://#{host}",
+                  :token_url => '/oauth/access_token')
+    token = client.client_credentials.get_token.token
+    @base = "https://#{host}/v1"
+    @header = {'Authorization' => "Bearer #{token}"}
+  end
+  def get(url)
+    RestClient.get("#{@base}/#{url}", @header){|resp, req, res, &block|
+    if (200..499).include? resp.code
+      resp
+    else
+      resp.return!(req, res, &block)
+    end
+    }
+  end
+end
+
+# setup the account specific API-Client key/secret and
+# save these in a dot file like ~/.halo
+# reference the location as a ENV param instead
+# of "hardcoding" them into this script (which may end up
+# in a repo by mistake)
+#
+# the format of the yaml file ie ~/.halo
+# halo:
+#   key_id : XXXXXXXX
+#   secret_key : XXXXXXXXXXXXXXXXXXXXXXXXXXX
+#
+# don't forget to add and export HALO_API_KEY_FILE
+# in your ~/.bash_profile Should look something like
+# HALO_API_KEY_FILE="/home/ehoffmann/.halo"
+# export HALO_API_KEY_FILE
+api_keys = YAML.load_file("#{ENV['HALO_API_KEY_FILE']}")
+key_id = api_keys['halo']['key_id']
+secret_key = api_keys['halo']['secret_key']
+
+# pass in a single or list of CVEs to check for
+options = {:cve => nil}
+parser = OptionParser.new do |opts|
+  opts.on('-c', '--cve ', 'single CVE or comma separated list') do |cve|
+    options[:cve] = cve
+  end
+  opts.on('-h', '--help', 'usage: check_for_cves.rb --cve "CVE-2014-0160,CVE-2014-1912"') do
+    puts opts
+    exit
+  end
+end
+parser.parse!
+
+# setup our API client
+host = 'api.cloudpassage.com'
+@api = API.new(key_id, secret_key, host)
+
+# search for active servers
+resp = @api.get("/servers?state=active")
+data = JSON.parse(resp)
+
+# setup a header
+puts "hostname, connecting_ip_addr, platform, platform_version, pkg_name, pkg_version, cve"
+
+# iterate through each server, grab its issues from
+# the last scheduled or manually launched scan
+data['servers'].each do |srv|
+  # srv['reported_fqdn'] is available too
+  host = "#{srv['hostname']}, #{srv['connecting_ip_address']}"
+  platform = "#{srv['platform']}, #{srv['platform_version']}"
+  i = @api.get("/servers/#{srv['id']}/svm")
+  d = JSON.parse(i)
+
+  begin # only look at vulnerability issues
+    d['scan']['findings'].each do |detail|
+      # iterate through each CVE
+      detail['cve_entries'].each do |cve|
+        if options[:cve].downcase.include?(cve['cve_entry'].downcase) # options passed in on cmdline
+          pkg = "#{detail['package_name']}, #{detail['package_version']}, #{cve['cve_entry']}"
+          puts "#{host}, #{platform}, #{pkg}"
+        end
+      end
+    end
+  rescue => e
+    # no vuln scan has completed
+  end
+end
+
+puts "Checked #{data['servers'].length} servers for #{options[:cve]}"
